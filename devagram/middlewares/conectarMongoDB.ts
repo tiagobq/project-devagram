91b75e14839a44e25c39c20eaabae711997f18f2
Tiago Sousa <159965545+tiagobq@users.noreply.github.com>
2024-12-01 20:44:14 -0300

    middlewares

---
+++ devagram/middlewares/conectarMongoDB.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse, NextApiHandler } from "next";
+import mongoose from 'mongoose';
+
+export const conectarMongoDB = (handler : NextApiHandler) =>
+async (req: NextApiRequest, res : NextApiResponse) => {
+
+    // verificar se o banco ja esta conectado, se estiver seguir
+    // para o endpoint ou proximo middleware
+    if(mongoose.connections[0].readyState){
+        return handler(req, res);
+    }
+
+    //ja que nao esta conectado vamos conectar
+    // obter a variavel de ambiente preenchida do env
+    const {DB_CONEXAO_STRING} = process.env;
+
+    // se a env estiver vazia aborta o uso do sistema e avisa o programador
+    if(!DB_CONEXAO_STRING){
+        return res.status(500).json({ erro : 'ENV de configuracao do banco, nao informado'});
+    }
+    mongoose.connection.on('connected', () => console.log('Banco de dados conectado'));
+    mongoose.connection.on('error', error => console.log(`ocorreu erro ao conectar no banco de dados`))
+    await mongoose.connect(DB_CONEXAO_STRING);
+
+}
