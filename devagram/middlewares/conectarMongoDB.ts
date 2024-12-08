99c6bb2e13ae519520c28bbb67e55d33e257e20f
Tiago Sousa <159965545+tiagobq@users.noreply.github.com>
2024-12-08 15:22:21 -0300

    salvar usuario e email

--- devagram/middlewares/conectarMongoDB.ts
+++ devagram/middlewares/conectarMongoDB.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse, NextApiHandler } from "next";
 import mongoose from 'mongoose';
-import type {RespostaPadraoMsg} from '../types/RespostaPadrãoMsg';
+import type {RespostaPadraoMsg} from '../types/RespostaPadraoMsg';
 
 export const conectarMongoDB = (handler : NextApiHandler) =>
 async (req: NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
