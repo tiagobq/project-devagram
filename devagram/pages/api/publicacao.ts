fb0cd7f89459fc56e3c694520201f15d494f8a74
Tiago Sousa <159965545+tiagobq@users.noreply.github.com>
2025-01-10 20:59:03 -0300

    submit

--- devagram/pages/api/publicacao.ts
+++ devagram/pages/api/publicacao.ts
@@ -4,19 +4,54 @@ import { conectarMongoDB } from "@/middlewares/conectarMongoDB"; // Importando o
 import type { RespostaPadraoMsg } from "@/types/RespostaPadraoMsg"; // Importando o tipo de resposta padrão que criamos
 import { upload, uploadImagemCosmic } from "@/services/uploadImagemCosmic";
 import nc from 'next-connect';
+import {PublicacaoModel} from '../../models/PublicacaoModel';
+import {UsuarioModel} from '../../models/UsuarioModel';
 
 const handler = 
     nc() 
     .use(upload.single("file")) 
     .post(async (req : any, res: NextApiResponse<RespostaPadraoMsg>) => {
-        const {descricao, file} = req.body;
+        
+        try{ 
+            const {userId} = req.query;
+            const usuario = await UsuarioModel.findById(userId);
+            if(!usuario){
+                return res.status(400).json({erro: 'Usuario nao encontrado'});
+            }
+            if(!req || !req.body){
+                return res.status(400).json({erro: 'Parametros de entrada não informados'});
+            }
+            const {descricao} = req?.body;
 
         if(!descricao || descricao.length < 2){
             return res.status(400).json({erro: 'Descricao nao e valida'});
         }
 
-        if(!file){
+        if(!req.file || !req.file.originalname){
             return res.status(400).json({erro: 'Imagem e obrigatoria'});
         }
-        return res.status(200).json({msg: 'Publicacao esta valida'});
-    })
+
+        const image = await uploadImagemCosmic(req);
+        const publicacao = {
+            idUsuario : usuario._id,
+            descricao,
+            foto : image.media.url,
+            data : new Date()
+        }
+
+        await PublicacaoModel.create(publicacao);
+
+        return res.status(200).json({msg: 'Publicacao criada com sucesso'});
+    }catch (e) {
+        console.log(e);
+            return res.status(400).json({erro: 'Erro ao cadastrar publicação'});
+        }
+    });
+
+    export const config = {  // Passa uma configuração para o NEXT não transformar a resposta em JSON e enviar a request como FormData devido a estarmos passando um arquivo
+        api : {
+            bodyParser : false
+        }
+    };
+
+    export default validarTokenJWT(conectarMongoDB(handler));
