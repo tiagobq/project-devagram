c626f266cee788a409214a4284bc5161d919bd95
Tiago Sousa <159965545+tiagobq@users.noreply.github.com>
2025-02-07 21:55:51 -0300

    seguir

--- devagram/pages/api/seguir.ts
+++ devagram/pages/api/seguir.ts
@@ -4,12 +4,36 @@ import { validarTokenJWT } from "@/middlewares/validarTokenJWT";
 import { conectarMongoDB } from "@/middlewares/conectarMongoDB";
 import { PublicacaoModel } from "@/models/PublicacaoModel";
 import { UsuarioModel } from "@/models/UsuarioModel";
+import { SeguidorModel } from "@/models/SeguidorModel";
 
-const endpointSeguir = (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
+const endpointSeguir = async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
     try{
         if(req.method === 'PUT'){
-            // quais dados vamos receber e aonde?
-            // id do usuario vindo do token
+
+            const {userId, id} = req?.query;
+
+            // usuario logado/autenticado = quem esta fazendo as acoes
+            const usuarioLogado = await UsuarioModel.findById(userId);
+            if(!req?.query?.userId){
+                return res.status(400).json({erro : 'usuario logado nao encontrado'});
+
+        }
+        // id do usuario e ser seguidor - query
+        const usuarioASerSeguido = await UsuarioModel.findById(id);
+        if(req?.query?.id){
+            return res.status(400).json({ erro : 'usuario a ser seguido nao encontrado'});
+        }
+
+        // inserir registro???
+        // e se ela ja segue??
+
+        const euJaSigoEsseUsuario = await SeguidorModel
+        .find({usuarioId: usuarioLogado._id, usuarioSeguidoId : usuarioASerSeguido._id});
+        if(euJaSigoEsseUsuario && euJaSigoEsseUsuario.length >0){
+            // sinal que eu ja sigo esse usuario
+        }else{
+            // sinal que eu nao sigo esse usuario
+        }
         }
         return res.status(405).json({erro: 'metodo informado nao existe'});
     }catch(e){
