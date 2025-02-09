c401b3b4988d8b094d1bcfa747766eb795a25e92
Tiago Sousa <159965545+tiagobq@users.noreply.github.com>
2025-02-09 18:35:57 -0300

    deixar-de-seguir

--- devagram/pages/api/seguir.ts
+++ devagram/pages/api/seguir.ts
@@ -14,25 +14,44 @@ const endpointSeguir = async (req : NextApiRequest, res : NextApiResponse<Respos
 
             // usuario logado/autenticado = quem esta fazendo as acoes
             const usuarioLogado = await UsuarioModel.findById(userId);
-            if(!req?.query?.userId){
+            if(!usuarioLogado){
                 return res.status(400).json({erro : 'usuario logado nao encontrado'});
 
         }
         // id do usuario e ser seguidor - query
         const usuarioASerSeguido = await UsuarioModel.findById(id);
-        if(req?.query?.id){
+        if(!usuarioASerSeguido){
             return res.status(400).json({ erro : 'usuario a ser seguido nao encontrado'});
         }
 
-        // inserir registro???
-        // e se ela ja segue??
-
+        // buscar se eu logado sigo ou nao esse usuario
         const euJaSigoEsseUsuario = await SeguidorModel
         .find({usuarioId: usuarioLogado._id, usuarioSeguidoId : usuarioASerSeguido._id});
         if(euJaSigoEsseUsuario && euJaSigoEsseUsuario.length >0){
             // sinal que eu ja sigo esse usuario
+            euJaSigoEsseUsuario.forEach(async(e : any) => await SeguidorModel.findByIdAndDelete({_id : e._id}))
+            usuarioLogado.seguindo--;
+            await UsuarioModel.findByIdAndUpdate({_id : usuarioLogado._id}, usuarioLogado);
+            usuarioASerSeguido.seguidores--;
+            await UsuarioModel.findByIdAndUpdate({_id : usuarioASerSeguido._id}, usuarioASerSeguido);
+
+            return res.status(200).json({msg : 'deixou de seguir o usuario com sucesso'});
         }else{
             // sinal que eu nao sigo esse usuario
+            const seguidor = {
+                usuarioId : usuarioLogado._id,
+                usuarioSeguidoId : usuarioASerSeguido._id
+            };
+            await SeguidorModel.create(seguidor);
+            // adicionar um seguindo no usuario logado
+            usuarioLogado.seguindo++;
+            await UsuarioModel.findByIdAndUpdate({_id : usuarioLogado._id}, usuarioLogado);
+
+            //adicionar um seguidor no usuario seguido
+            usuarioASerSeguido.seguidores++;
+            await UsuarioModel.findByIdAndUpdate({_id : usuarioASerSeguido._id}, usuarioASerSeguido);
+
+            return res.status(200).json({msg : 'usuario seguido com sucesso'});
         }
         }
         return res.status(405).json({erro: 'metodo informado nao existe'});
